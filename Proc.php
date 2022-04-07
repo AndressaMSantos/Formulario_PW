c9d3ef6dfd2a1277760683b3546f9f13e8f03f8f
Andressa Santos <100451911+AndressaMSantos@users.noreply.github.com>
2022-04-06 22:34:55 -0300

    Update Proc.php

--- Proc.php
+++ Proc.php
@@ -2,73 +2,59 @@
 
     //Variáveis Globais
 
-    $nome = $_GET['nome'];
-    $sexo = $_GET['sexo'];
-    $senha = $_GET['senha'];
-    $email = $_GET['email'];
-    $telefone = $_GET['telefone'];
-    $arq = $_GET['arq'];
+   echo  $nome = $_POST['nome'];
+    $sexo = $_POST['sexo'];
+    $senha = $_POST['senha'];
+    $email = $_POST['email'];
+    $telefone = $_POST['telefone'];
+    $arq = $_FILES['arq'];
 
-    // $nome = $_POST['nome'];
-    // $sexo = $_POST['sexo'];
-    // $senha = $_POST['senha'];
-    // $email = $_POST['email'];
-    // $telefone = $_POST['telefone'];
+   
 
-    //Imprimir Variáveis
-    echo "<p><strong> Nome: " . $nome . "</strong></p>";
-    echo "<p><strong> E-mail: " . $email . "</strong></p>";
-    echo "<p><strong> Telefone: " . $telefone . "</strong></p>";
-    echo "<p><strong> Sexo: " . $sexo . "</strong></p>";
-    echo "<p><strong> Senha: " . $senha . "</strong></p>";
-    echo "<p><strong> Arquivo: " . $arq . "</strong></p>";
-    
+    // S
     
 
-        // echo strlen($nome); // Mostra na tela a quantidade de caracter
-        // echo "<br>";
-        // echo empty($nome); //Usando (int) antes do empty, ele mostra o valor do bolean número 0 na tela
-        // echo "<br>";
-
-    if( strlen($nome)==0 )
+        if( strlen($nome) == 0 )
     {
-        //echo " Campo Obrigatório!!! ";
+      
         header("location:Formulario.php?erro=nome");
+        return;
     }
-    if( strlen($senha)<= 6 )
-    {
-        //echo " Coloque uma senha de 6 caracteres ";
-        header("location:Formulario.php?erro=senha");
-    }
+
+
     if( strlen($email)== 0 )
     {
-        //echo " Por favor, coloque um endereço de e-mail ";
+        
         header("location:Formulario.php?erro=email");
+        return;
     }
+    
+    
     if( strlen($telefone)== 0 )
     {
-        //echo " Adicione um Número de Telefone, exemplo: 15 98765-4321";
+        
         header("location:Formulario.php?erro=telefone");
+        return;
     }
-    if( strlen($arq)==0 )
+    
+    
+    if( strlen($senha)<= 6 )
     {
-        //echo " Adicione um Número de Telefone, exemplo: 15 98765-4321";
-        header("location:Formulario.php?erro=arq");
+        
+        header("location:Formulario.php?erro=senha");
+        return;
     }
+    
+    
+    // if( strlen($arq)==0 )
+    // {
+      
+    //     header("location:Formulario.php?erro=arq");
+    // }
 
 
-    //Upload - Servidor
-
-    //var_dump($_FILES['arq']);
-    // echo "<p> Nome do arquivo: " . $_FILES["arq"]["name"] . "</p>";
-    // echo "<p> Tipo do arquivo: " . $_FILES["arq"]["type"] . "</p>";
-    // echo "<p> Erro do arquivo: " . $_FILES["arq"]["error"] . "</p>";
-    // echo "<p> Diretorio Temporario: " . $_FILES["arq"]["tmp_name"] . "</p>";
-    // echo "<p> Tamanho do arquivo: " . $_FILES["arq"]["size"] . "</p>"; 
-
-    //Mover da pasta Temporaria, para a pasta do Servidor
     $tmp = $_FILES['arq']['tmp_name'];
-    $pasta = "arq" . $_FILES['arq']['name'];
+    $pasta = "arq/" . $_FILES['arq']['name'];
     move_uploaded_file($tmp , $pasta);
     echo (int) move_uploaded_file($tmp , $pasta);
 ?>
