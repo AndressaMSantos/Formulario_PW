5f43d2cc776283d1391a1a7850807fe9f96b4a28
Andressa Santos <100451911+AndressaMSantos@users.noreply.github.com>
2022-04-06 22:33:16 -0300

    Update Formulario.php

--- Formulario.php
+++ Formulario.php
@@ -4,45 +4,32 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Preencher Formulario</title>
 </head>
-<body>
-    <!-- get - Dados via URL 
-         post - Dados Ocultos 
-         request - Ambos Dados
+<body  style="background-Color:purple">  
 
-         $_FILES
-         move_uploaded_file();
+    
 
-         Arquivo => Informações
-
-         ['name']
-         ['type']
-         ['tmp_name']
-         ['error']
-         -upload.max.filesize - tamanho do arquivo máximo (php.ini)
-         ['size']
-     -->
-
-    <form action="Proc.php" method="get" enctype="multipart/form-data">
+    <form action="Proc.php" method="Post" enctype="multipart/form-data">
 
         <div>
-
-            <label> Nome:</label>
-            <input type= "text" id="nome" name="nome"/>
+             <p>
+            <label style="color:white"> Nome:</label>
+            <input type= "text" id="nome" name="nome" color="White"/>
             <?php
                 if(isset($_GET['erro']) && $_GET['erro'] == "nome")
                 {
                     echo "<span style=\"color:red\"> Campo Obrigatório! </span>";
                 }
             ?>
+            </p>
             <!-- <span>*</span> -->
 
         </div>
 
         <div>
 
-            <label> E-mail:</label>
+            <label style="color:white"> E-mail:</label>
             <input type= "text" id="email" name="email"/>
             <?php
                 if(isset($_GET['erro']) && $_GET['erro'] == "email")
@@ -54,8 +41,8 @@
         </div>
 
         <div>
-
-            <label> Telefone:</label>
+            <p>
+            <label style="color:white"> Telefone:</label>
             <input type= "text" id="telefone" name="telefone"/>
             <?php
                 if(isset($_GET['erro']) && $_GET['erro'] == "telefone")
@@ -63,21 +50,21 @@
                     echo "<span style=\"color:red\"> Adicione um Número de Telefone, exemplo: 15 98765-4321 </span>";
                 }
             ?>
-
+            </P>
         </div>
         
 
         <div> 
-
-            <label> Sexo:</label>
+            <p>
+            <label style="color:white"> Sexo:</label>
             <input type="radio" id="sexo" checked name="sexo" value="F"/> Feminino
             <input type="radio" id="sexo" name="sexo" value="M"/> Masculino
-
+            </p>
         </div>
 
         <div>
-
-            <label> Senha:</label>
+            <p>
+            <label style="color:white"> Senha:</label>
             <input type="password" id="senha" name="senha"/>
             <?php
                 if(isset($_GET['erro']) && $_GET['erro'] == "senha")
@@ -85,10 +72,12 @@
                     echo "<span style=\"color:red\"> Senha, precisa conter no minímo 6 caracteres </span>";
                 }
             ?>
+            </p>
         </div>
 
         <div>
-            <label> Adicionar Arquivos: </label>
+            <p>
+            <label style="color:white"> Adicionar Arquivos: </label>
             <input type="file" id="arq" name="arq"/>
             <?php
                 if(isset($_GET['erro']) && $_GET['erro'] == "arq")
@@ -96,13 +85,14 @@
                     echo "<span style=\"color:red\"> Adicione um Arquivo </span>";
                 }
             ?>
+            </p>
 
 
 
         </div>
-
+        <p>
         <input type="submit" value="cadastro"/>
-    
+        <p>
     </form>
 
 </body>
